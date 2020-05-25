92433f84e6e7f3299479292df80255464352bef9
Ubuntu <student@ML-RefVm-530040.2p1jpcpss51uxaav52dox2fqhh.qx.internal.cloudapp.net>
2020-05-25 13:35:05 +0000

    2.2 setPassword.sh script added

---
+++ scripts/portfolio/week2/setPassword.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# setPassword.sh
+#   script to ask user for a password and securely save it 
+
+# get directory from user
+read -p "Type the name of the folder you would like save the password in? " folderName
+echo
+
+# check if folder already exists and exit
+if [ -d "$folderName" ]; then
+    echo Folder already exists exiting..
+    exit 1
+fi
+ 
+# Create the folder
+mkdir "$folderName"
+
+# set permissions on all created files to only allow user to read/write all other groups have no access
+setfacl -d -m u::rwx $folderName   # set user to full rights
+setfacl -d -m g::--x $folderName   # revoke all group rights
+setfacl -d -m o::--- $folderName   # revoke all others permissions
+
+# retrieve users password
+read -p "Please enter the password to save: " userPassword
+echo
+
+# write the clear text passsword to secret.txt
+echo $userPassword | sha256sum > "$folderName/secret.txt"
+
+echo "Password hash successfully written to $folderName/secret.txt"
+echo
+exit 0
